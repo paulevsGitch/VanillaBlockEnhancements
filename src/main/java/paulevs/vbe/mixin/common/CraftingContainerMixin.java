719edaf8dce2555555c0832001a701af79342c4d
paulevsGitch <paulevs@yandex.ru>
2023-07-31 03:14:11 +0300

    No drop items from workbench on screen close, no breaking farmland on walking

---
+++ src/main/java/paulevs/vbe/mixin/common/CraftingContainerMixin.java
@@ -0,0 +1,39 @@
+package paulevs.vbe.mixin.common;
+
+import net.minecraft.container.Crafting;
+import net.minecraft.entity.player.PlayerBase;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Crafting.class)
+public class CraftingContainerMixin {
+	@Shadow public net.minecraft.inventory.Crafting inventory;
+	@Shadow private Level level;
+	
+	@Inject(method = "onClosed", at = @At("HEAD"), cancellable = true)
+	public void vbe_onClosed(PlayerBase player, CallbackInfo info) {
+		info.cancel();
+		if (this.level.isClientSide) return;
+		PlayerInventory inventory = player.inventory;
+		vbe_addOrDrop(player, inventory.getCursorItem());
+		inventory.setCursorItem(null);
+		for (byte index = 0; index < 9; index++) {
+			vbe_addOrDrop(player, this.inventory.getInventoryItem(index));
+		}
+	}
+	
+	@Unique
+	private void vbe_addOrDrop(PlayerBase player, ItemStack stack) {
+		if (stack == null) return;
+		if (!player.inventory.addStack(stack)) {
+			player.dropItem(stack);
+		}
+	}
+}
---
+++ src/main/java/paulevs/vbe/mixin/common/FarmlandBlockMixin.java
@@ -0,0 +1,17 @@
+package paulevs.vbe.mixin.common;
+
+import net.minecraft.block.FarmlandBlock;
+import net.minecraft.entity.BaseEntity;
+import net.minecraft.level.Level;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(FarmlandBlock.class)
+public class FarmlandBlockMixin {
+	@Inject(method = "onSteppedOn", at = @At("HEAD"), cancellable = true)
+	public void vbe_onSteppedOn(Level level, int x, int y, int z, BaseEntity entity, CallbackInfo info) {
+		info.cancel();
+	}
+}

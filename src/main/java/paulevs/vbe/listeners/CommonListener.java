99292700425af3a68f5c7ab1c9f5874cd27367fe
paulevsGitch <paulevs@yandex.ru>
2023-10-23 10:37:04 +0300

    Fix for missing stairs shapes

--- src/main/java/paulevs/vbe/listeners/CommonListener.java
+++ src/main/java/paulevs/vbe/listeners/CommonListener.java
@@ -3,6 +3,7 @@ package paulevs.vbe.listeners;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.event.registry.BlockRegistryEvent;
 import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
+import paulevs.vbe.block.StairsShape;
 import paulevs.vbe.block.VBEBlocks;
 import paulevs.vbe.item.VBEItems;
 
@@ -11,6 +12,7 @@ public class CommonListener {
 	
 	@EventListener
 	private void onBlockRegister(BlockRegistryEvent event) {
+		StairsShape.init();
 		VBEBlocks.init();
 		blockRegistered = true;
 	}

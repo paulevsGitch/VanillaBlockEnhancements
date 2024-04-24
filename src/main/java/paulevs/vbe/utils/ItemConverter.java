c9d239965a52b319fdeb3a8435bd0f27bb0fb0e7
paulevsGitch <paulevs@yandex.ru>
2024-04-25 00:00:59 +0300

    Item ID fix

--- src/main/java/paulevs/vbe/utils/ItemConverter.java
+++ src/main/java/paulevs/vbe/utils/ItemConverter.java
@@ -8,11 +8,11 @@ import paulevs.vbe.item.VBEItems;
 
 public class ItemConverter {
 	public static int getID(int id, int damage) {
-		if (id == Block.STONE_SLAB.id && VBE.ENHANCED_SLABS.getValue()) return VBEBlocks.getHalfSlabByMeta(damage).id;
-		if (id == Block.LOG.id) return VBEBlocks.getLogByMeta(damage).id;
-		if (id == Block.LEAVES.id) return VBEBlocks.getLeavesByMeta(damage).id;
-		if (id == Item.woodDoor.id) return VBEItems.OAK_DOOR.id;
-		if (id == Item.ironDoor.id) return VBEItems.IRON_DOOR.id;
+		if (id == Block.STONE_SLAB.id && VBE.ENHANCED_SLABS.getValue()) return VBEBlocks.getHalfSlabByMeta(damage).asItem().id;
+		if (id == Block.LOG.id) return VBEBlocks.getLogByMeta(damage).asItem().id;
+		if (id == Block.LEAVES.id) return VBEBlocks.getLeavesByMeta(damage).asItem().id;
+		if (id == Item.woodDoor.id) return VBEItems.OAK_DOOR.asItem().id;
+		if (id == Item.ironDoor.id) return VBEItems.IRON_DOOR.asItem().id;
 		return -1;
 	}
 }

4e13232e06602ac7aa74dad996329a9c6fbac0f5
paulevsGitch <paulevs@yandex.ru>
2025-05-11 04:07:10 +0300

    Fixed corruption of old worlds

--- src/main/java/paulevs/vbe/mixin/common/FlattenedWorldManagerMixin.java
+++ src/main/java/paulevs/vbe/mixin/common/FlattenedWorldManagerMixin.java
@@ -1,14 +1,19 @@
 package paulevs.vbe.mixin.common;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.Block;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.StairsBlock;
 import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.level.Level;
 import net.minecraft.level.chunk.Chunk;
 import net.minecraft.util.io.CompoundTag;
+import net.minecraft.util.io.ListTag;
 import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.api.registry.BlockRegistry;
 import net.modificationstation.stationapi.api.state.property.Properties;
+import net.modificationstation.stationapi.api.state.property.Property;
+import net.modificationstation.stationapi.api.util.Identifier;
 import net.modificationstation.stationapi.api.util.math.Direction;
 import net.modificationstation.stationapi.impl.world.FlattenedWorldManager;
 import net.modificationstation.stationapi.impl.world.chunk.ChunkSection;
@@ -20,6 +25,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import paulevs.vbe.VBE;
 import paulevs.vbe.block.VBEBlockProperties;
 import paulevs.vbe.block.VBEBlockProperties.TopBottom;
+
+import java.util.Collection;
 
 @Mixin(FlattenedWorldManager.class)
 public class FlattenedWorldManagerMixin {
@@ -82,6 +89,35 @@ public class FlattenedWorldManagerMixin {
 					dx, dy, dz,
 					state.with(Properties.HORIZONTAL_FACING, dir).with(VBEBlockProperties.OPENED, open)
 				);
+			}
+		}
+	}
+	
+	@Inject(method = "loadChunk", at = @At(
+		value = "INVOKE",
+		target = "Lnet/minecraft/level/Level;sectionCoordToIndex(I)I",
+		shift = Shift.AFTER
+	))
+	private static void vbe_fixBlocksWithoutProperties(
+		Level level, CompoundTag chunkTag, CallbackInfoReturnable<Chunk> info,
+		@Local(name = "sectionTag") CompoundTag sectionTag
+	) {
+		if (!sectionTag.containsKey("block_states")) return;
+		CompoundTag states = sectionTag.getCompoundTag("block_states");
+		ListTag palette = states.getListTag("palette");
+		for (short i = 0; i < palette.size(); i++) {
+			CompoundTag tag = (CompoundTag) palette.get(i);
+			if (tag.containsKey("Properties")) continue;
+			Identifier id = Identifier.of(tag.getString("Name"));
+			Block block = BlockRegistry.INSTANCE.get(id);
+			if (block == null) continue;
+			Collection<Property<?>> properties = block.getStateManager().getProperties();
+			if (properties.isEmpty()) continue;
+			BlockState state = block.getDefaultState();
+			CompoundTag propertiesTag = new CompoundTag();
+			tag.put("Properties", propertiesTag);
+			for (Property<?> property : state.getProperties()) {
+				propertiesTag.put(property.getName(), state.get(property).toString());
 			}
 		}
 	}
